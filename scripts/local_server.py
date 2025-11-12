a9731abed5a10f4747e9a28b71142bd9c2a6ebe2
hougaardstenius-spec <hougaardstenius@gmail.com>
2025-11-12 15:33:22 +0100

    Delete scripts directory

--- scripts/local_server.py
+++
@@ -1,8 +0,0 @@
-
-import http.server, socketserver, sys, os
-PORT = int(sys.argv[1]) if len(sys.argv)>1 else 8080
-os.chdir(os.path.dirname(os.path.abspath(__file__)) + '/..')
-Handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving on http://localhost:{PORT}")
-    httpd.serve_forever()
